b9e9721fe2ff14b417d986d1619c572411ba4fb9
LAPTOP-GQGBUH1P\mick <mick@gerritsen-online.nl>
2019-03-06 10:01:13 +0100

    Changes

---
+++ AppliedGameJam/Assets/Accupances.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accupances : MonoBehaviour {
+
+    public int maximumAccupanceAmount;
+    public int AccupanceAmount;
+    public Transform[] workerPositions;
+    
+
+	public Transform GetNewAccupanceTransform() {
+        if (AccupanceAmount < maximumAccupanceAmount) {
+            return workerPositions[AccupanceAmount];
+        } else return null;
+    }
+}
